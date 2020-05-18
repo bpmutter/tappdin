6ab014a8a2170cb2fc6babeba6a67199f6fbc0ad
jank_ <jank_329@hotmail.com>
2020-05-18 16:57:15 -0500

    Creating lists and lickedBreweries routes

---
+++ tappdin-backend/routes/likedBreweries.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const db = require("../db/models");
+
+const {check, validationResult} = require('express-validator');
+const {
+    asyncHandler,
+    handleValidationErrors
+} = require('./utils')
+const { requireAuth } = require("../auth");
+
+router.use(requireAuth);
+
+
+
+router.get("/liked-breweries", asyncHandler(async (req, res) => {
+    const likedBreweries = await db.LikedBrewery.findAll({
+        include: [db.User, db.Brewery]
+    });
+    res.json({
+        likedBreweries
+    });
+}));
+
+module.exports = router;
