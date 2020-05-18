22e2ada04c30fbecb561ea01a6c65ee8620b3416
jank_ <jank_329@hotmail.com>
2020-05-18 16:44:11 -0500

    Creating checkin routes

--- tappdin-backend/routes/beers.js
+++ tappdin-backend/routes/beers.js
@@ -19,9 +19,18 @@ router.get("/beers", asyncHandler(async(req, res)=>{
     res.json({beers})
 }));
 
+router.get("/beers/top", asyncHandler(async(req, res)=>{
+        const topBeers = await db.Beer.findAll({
+            include: [db.Beer, db.Checkin.rating, db.Brewery],
+            order: [["rating", "DESC"]],
+
+        });
+        res.json({topBeers});
+    }));
+
 router.get("/beers/:id(\\d+)", asyncHandler(async(req, res)=>{
     const beerId = parseInt(req.params.id,10);
-    const beer = await Beer.findByPk(beerId);
+    const beer = await db.Beer.findByPk(beerId);
     res.json({
         beer
     });
@@ -30,9 +39,9 @@ router.get("/beers/:id(\\d+)", asyncHandler(async(req, res)=>{
 router.delete("/beers/:id(\\d+)",
     asyncHandler(async (req, res) => {
         const beerId = parseInt(req.params.id, 10);
-        const beer = await Tweet.findByPk(tweetId);
+        const beer = await db.Beer.findByPk(beerId);
 
-        const deletedBeer = await tweet.destroy();
+        const deletedBeer = await beer.destroy();
 
         res.json({msg: "The beer is no longer available 😞!",deletedBeer});
 }));
