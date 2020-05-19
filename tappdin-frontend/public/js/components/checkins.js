cc4af1d38ba321cb00272297b668fb8f4e3a42f2
Ben Perlmutter <benperlmutter94@gmail.com>
2020-05-19 12:36:54 -0500

    finish prototype of render checkin view

---
+++ tappdin-frontend/public/js/components/checkins.js
@@ -0,0 +1,14 @@
+const ratings = document.querySelectorAll(".checkin__rating-val");
+console.log(ratings)
+
+for(let rating of ratings){
+    console.log(rating)
+    const ratingVal = parseInt(rating.dataset.rating, 10);
+    let displayRating = "";
+    for (let i = 1; i <= ratingVal; i++) {
+        displayRating += "🍺";
+        rating.innerHTML = displayRating;
+}
+
+}
+
