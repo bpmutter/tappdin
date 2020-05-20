e9107637ccd95dcd85ecf05afc113249227d59c6
jank_ <jank_329@hotmail.com>
2020-05-20 13:24:56 -0500

    Beers,breweries,checkins,users seed

--- tappdin-backend/generate-seed/seed-maker.js
+++ tappdin-backend/generate-seed/seed-maker.js
@@ -12,37 +12,13 @@ seed.forEach(beer => {
 });
 
 const brewerySeed = [];
-seed.forEach(beer => {
-    if (beer.breweries && beer.breweries[0].id) {
-        if (breweriesSet.has(beer.breweries[0].id)) {
-            //brewerySeed.push({
-            const breweryData ={
-                key: beer.breweries[0].id,
-                name: beer.breweries[0].name,
-                location: `${beer.breweries[0].locations[0].locality}, ${beer.breweries[0].locations[0].region}`,
-                description: beer.breweries[0].description,
-                website: beer.breweries[0].website,
-                // facebook: beer.breweries[0].id,
-                // instagram: beer.breweries[0].id,
-                // twitter: beer.breweries[0].id,
-                image: beer.breweries[0].images ? beer.breweries[0].images.squareLarge : null,
-                createdAt: new Date(),
-                updatedAt: new Date()
-            }
-            brewerySeed.push(breweryData);
-            //});
-            breweriesSet.delete(beer.breweries[0].id);
-        }
-    }
-});
 
 seed.forEach(beer => {
     if (beer.breweries && beer.breweries[0].id) {
         if (breweriesSet.has(beer.breweries[0].id)) {
-            //brewerySeed.push({
-            const breweryData ={
-                key: beer.breweries[0].id,
+            brewerySeed.push({
                 name: beer.breweries[0].name,
+                key: beer.breweries[0].id,
                 location: `${beer.breweries[0].locations[0].locality}, ${beer.breweries[0].locations[0].region}`,
                 description: beer.breweries[0].description,
                 website: beer.breweries[0].website,
@@ -52,9 +28,7 @@ seed.forEach(beer => {
                 image: beer.breweries[0].images ? beer.breweries[0].images.squareLarge : null,
                 createdAt: new Date(),
                 updatedAt: new Date()
-            }
-            brewerySeed.push(breweryData);
-            //});
+            });
             breweriesSet.delete(beer.breweries[0].id);
         }
     }
@@ -65,12 +39,12 @@ seed.forEach(beer=>{
     const beerData = {
         key: beer.id,
         name: beer.name,
-        abv: beer.abv || null,
+        abv: parseFloat(beer.abv) || null,
         image: (beer.labels  && beer.labels.large ) ? beer.labels.large : null,
-        categoryId: (beer.style && beer.style.category) ? beer.style.category.id : null,
-        ibu: (beer.style && beer.style.ibuMax) ? beer.style.ibuMax : null,
+        beerTypeId: (beer.style && beer.style.category) ? parseInt(beer.style.category.id) : 5,
+        ibu: (beer.style && beer.style.ibuMax) ? parseInt(beer.style.ibuMax) : null,
         description: (beer.style && beer.style.description) ? beer.style.description : null,
-        breweryId: (beer.breweries && beer.breweries[0].id) ? brewerySeedKeys.indexOf(beer.breweries[0].id) + 1 : null,
+        breweryId: (beer.breweries && beer.breweries[0].id) ? beer.breweries[0].id : null,
         createdAt: new Date(),
         updatedAt: new Date()
     }
