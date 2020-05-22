3bd2ec0f819dfdca4f870dcf6bde3e16e36dc82f
Johnny <jbui923@gmail.com>
2020-05-21 17:04:14 -0700

    deleted doubles

--- tappdin-frontend/routes/utils 2.js
+++
@@ -1,9 +0,0 @@
-const { validationResult } = require("express-validator");
-
-const asyncHandler = (handler) => (req, res, next) =>
-  handler(req, res, next).catch(next);
-
-
-module.exports = {
-  asyncHandler,
-};
