7d668946dec9ff29065760070327ce94ed6784a1
Ben Perlmutter <benperlmutter94@gmail.com>
2020-05-25 11:51:41 -0500

    add demo user login and clean up public JS files

---
+++ public/js/demo-user.js
@@ -0,0 +1,5 @@
+document.getElementById("demo-login").addEventListener("click", (e)=>{
+    document.getElementById("email").value="demo@user.com";
+    document.getElementById("password").value="abc123";
+    document.getElementById("login").click();
+})
